908a69eaaaa63e026669f6460eb2da5c6210c395
Rus <de.mr44@gmail.com>
2022-02-02 17:26:11 +0500

    Added Responder class

---
+++ Patterns-ChainOfResponsibility/Responder.swift
@@ -0,0 +1,34 @@
+//
+//  Responder.swift
+//  Patterns-ChainOfResponsibility
+//
+//  Created by Ruslan on 02.02.2022.
+//
+
+import Foundation
+
+final class Responder {
+    
+    private var qa: [String: String]
+    private var next: Responder?
+    
+    init(qa: [String: String]) {
+        self.qa = qa
+    }
+    
+    func setNext(_ next: Responder) {
+        self.next = next
+    }
+    
+    func answer(for question: String) -> String {
+        if let answer = qa[question] {
+            print("--- Done!")
+            return answer
+        } else if let next = next {
+            print("--- Can't handle it! Processing to the next...")
+            return next.answer(for: question)
+        }
+        print("--- No answer!")
+        return "It can't be handled!"
+    }
+}
