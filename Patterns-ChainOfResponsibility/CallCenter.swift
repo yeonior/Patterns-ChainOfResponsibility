b6e2174eacdb37f32263aea69b7af28c9cb729a4
Rus <de.mr44@gmail.com>
2022-02-02 18:02:51 +0500

    Added CallCenter class

---
+++ Patterns-ChainOfResponsibility/CallCenter.swift
@@ -0,0 +1,25 @@
+//
+//  CallCenter.swift
+//  Patterns-ChainOfResponsibility
+//
+//  Created by Ruslan on 02.02.2022.
+//
+
+import Foundation
+
+final class CallCenter {
+    
+    private var specialist: Responder
+    private var manager: Responder
+    private var expert: Responder
+    
+    init(specialist: Responder, manager: Responder, expert: Responder) {
+        self.specialist = specialist
+        self.manager = manager
+        self.expert = manager
+    }
+    
+    func receiveCall(question: String) {
+        print("Answer: \(specialist.answer(for: question))")
+    }
+}
